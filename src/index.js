01be1e8a5db2d0485a7c196d13c1ebe9b83efe48
igor-provotorov <garynya@yandex.ru>
2018-02-25 17:27:02 +0300

    quadratic-equation/feature: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+
+    let arr = equation.split(' ');
+    /*console.log(equationSplitted);*/
+    let a,b,c, D, d, x1, x2, result = [];
+
+    a = parseInt(arr[0]);
+    b = parseInt(arr[3] + arr[4]);
+    c = parseInt(arr[7] + arr[8]);
+
+    D = (b * b) - 4 * a * c;
+    d = Math.round(Math.sqrt(D));
+    x1 = ((-b) + d) / (2 * a);
+    x2 = ((-b) - d) / (2 * a);
+
+    result.push(x1);
+    result.push(x2);
+
+    if (result[1] < result[0]) {
+
+        result[0] = result[0] + result[1];
+        result[1] = result[0] - result[1];
+        result[0] = result[0] - result[1];
+    }
+
+    return result;
 }
